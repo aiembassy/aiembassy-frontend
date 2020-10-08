34d3b140d2729afb901c558b89c1ec7b1d551d2e
Kacper Łukawski <lukawski.kacper@gmail.com>
2020-10-08 23:10:41 +0200

    Add loading events on a detailed list from the file as well

--- src/components/Modals/ModalEvents/EventsList/EventsList.tsx
+++ src/components/Modals/ModalEvents/EventsList/EventsList.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import ScrollBar from 'react-perfect-scrollbar';
 import Link from 'next/link';
+import events from '@shared/data/events';
 import {
     EventsListWrapper,
     Event,
@@ -15,46 +16,45 @@ import {
 } from './EventsList.styled';
 
 const EventsList = () => {
-    const itemsList = new Array(15).fill(1);
-
     return (
         <EventsListWrapper>
             <ScrollBar>
-                {itemsList.map((item, index) => (
-                    <Event key={index}>
-                        <EventImage>
-                            <Image src="/images/smogathon.png" />
-                        </EventImage>
-                        <EventContentWrapper>
-                            <EventDate>Styczeń 2020</EventDate>
-                            <EventTitle>Tutaj kolejna nazwa</EventTitle>
-                            <EventDescription>
-                                Lorem ipsum dolor sit amet, consectetur adLorem
-                                ipsum dolor sit amet, consectetur adipiscing
-                                elit. Ac tincidunt ut dictumst urna dictum
-                                nulla. ipiscing elit. Ac tincidunt ut dictumst
-                                urna dictum nulla.
-                            </EventDescription>
-                            <Link
-                                scroll={false}
-                                href="/events/[eventId]"
-                                as="/events/konferencja-1"
-                            >
-                                <EventLinkWrapper>
-                                    <EventLink
-                                        buttonType="TRANSPARENT"
-                                        iconType="IconArrowRight"
-                                        iconSize={20}
-                                        iconActiveColor={['green_hover']}
-                                        noPadding
-                                    >
-                                        więcej
-                                    </EventLink>
-                                </EventLinkWrapper>
-                            </Link>
-                        </EventContentWrapper>
-                    </Event>
-                ))}
+                {Object.keys(events).map((eventId) => {
+                    const event = events[eventId];
+                    const eventLinkLocation = `/events/${eventId}`;
+
+                    return (
+                        <Event key={eventLinkLocation}>
+                            <EventImage>
+                                <Image src={event.imagePath} />
+                            </EventImage>
+                            <EventContentWrapper>
+                                <EventDate>{event.date}</EventDate>
+                                <EventTitle>{event.shortTitle}</EventTitle>
+                                <EventDescription>
+                                    {event.shortDescription}
+                                </EventDescription>
+                                <Link
+                                    scroll={false}
+                                    href="/events/[eventId]"
+                                    as={eventLinkLocation}
+                                >
+                                    <EventLinkWrapper>
+                                        <EventLink
+                                            buttonType="TRANSPARENT"
+                                            iconType="IconArrowRight"
+                                            iconSize={20}
+                                            iconActiveColor={['green_hover']}
+                                            noPadding
+                                        >
+                                            więcej
+                                        </EventLink>
+                                    </EventLinkWrapper>
+                                </Link>
+                            </EventContentWrapper>
+                        </Event>
+                    );
+                })}
             </ScrollBar>
         </EventsListWrapper>
     );
