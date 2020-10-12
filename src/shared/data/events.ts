db5d60c73bb34b2db2f3f305bb4f6ea34ba8df3e
Kacper Łukawski <lukawski.kacper@gmail.com>
2020-10-12 20:54:34 +0200

    Add presentation info to GHOST Day 2020 description

--- src/shared/data/events.ts
+++ src/shared/data/events.ts
@@ -7,13 +7,23 @@ export default {
             'badaniach nad sztuczną inteligencją i nieszablonowym ' +
             'podejściu do statystyki.',
         imagePath: '/images/ghost_day.png',
-        date: 'Październik 2020',
+        date: '23-24. października 2020',
         title: 'GHOST Day: Applied Machine Learning Conference 2020',
         text:
-            '2 dni wykładów, 3 równoległe ścieżki, 4 keynote’y, 36 prezentacji i 350 uczestników to ' +
-            'druga edycja darmowego wydarzenia GHOST Day: Applied Machine Learning' +
-            'Conference organizowanego zdalnie przez KN GHOST z Politechniki Poznańskiej.\n\n' +
-            'Termin: 23-24 października 2020 r. (piątek, sobota).\n' +
-            'Charakter: Zdalny',
+            '<p><b>2 dni wykładów, 3 równoległe ścieżki, 4 keynote’y, 36 prezentacji i 350 uczestników to ' +
+            'druga edycja darmowego wydarzenia GHOST Day: Applied Machine Learning ' +
+            'Conference organizowanego zdalnie przez KN GHOST z Politechniki Poznańskiej.</b></p>' +
+            '<p>Termin: <b>23-24 października 2020 r. (piątek, sobota)</b></p>' +
+            '<p>Charakter: <b>Zdalny</b></p>' +
+            '<p>GHOST Day: AMLC jest wydarzeniem skupiającym się na praktycznych zastosowaniach ' +
+            'uczenia maszynowego, badaniach nad sztuczną inteligencją i nieszablonowym podejściu do n' +
+            'statystyki. Celem konferencji jest przede wszystkim integracja środowiska ' +
+            'badawczo-rozwojowego, promocja wiedzy oraz prezentacja wyników badań, wzmacnianie relacji ' +
+            'pomiędzy światem naukowym i biznesem, jak również wymiana wiedzy i doświadczeń między ' +
+            'praktykami zajmującymi się uczeniem maszynowym, sztuczną inteligencją czy analizą danych.</p>' +
+            '<p>W trakcie konferencji Kacper Łukawski wygłosi prelekcję &quot;SmogSpots. How machine ' +
+            'learning may help us to solve smog issue?&quot;. Do zobaczenia 23. października, 16:15-16:45.</p>' +
+            '<p><a href="https://ghostday.pl/index.php/speakers/#talk-622">' +
+            'https://ghostday.pl/index.php/speakers/#talk-622</a></p>',
     },
 };
