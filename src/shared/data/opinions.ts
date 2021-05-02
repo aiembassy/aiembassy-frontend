07ef438d4b69921b4292fd8bd5b816316cd20e9f
Kacper Łukawski <lukawski.kacper@gmail.com>
2021-05-02 13:24:26 +0200

    Translate opinions

--- src/shared/data/opinions.ts
+++ src/shared/data/opinions.ts
@@ -29,25 +29,21 @@ export default {
     en: [
         {
             text:
-                '“Rozdano nagrody w kategoriach:\n' +
-                '<br /> Identyfikacja Źródeł Zanieczyszczenia\n' +
-                'Powietrza (patron kategorii: Krakowski Alarm\n' +
-                'Smogowy) – Zespół <strong>AI Embassy</strong>:\n' +
-                'Innowacyjna Metoda analizy danych o\n' +
-                'zanieczyszczeniu korzystająca z danych\n' +
-                'satelitarnych oraz sztucznej inteligencji.”',
+                '“Awards were given in the following categories:\n' +
+                '<br /> Identification of Air Pollution Sources (category ' +
+                'patron: Krakow Smog Alert) - <strong>AI Embassy</strong> ' +
+                'Team: An innovative method of pollution data analysis using ' +
+                'satellite data and artificial intelligence.”',
             source:
                 'https://smogathon.com/pl/smogaton-2019-hackathon-z-wykorzystaniem-danych/',
             sourceName: 'smogathon.com.pl',
         },
         {
             text:
-                '“W kategorii Identyfikacja źródeł\n' +
-                'zanieczyszczenia powietrza najlepszy okazał się\n' +
-                'zespół <strong>AI Embassy</strong>. Przedstawił\n' +
-                'on innowacyjną metodę analizy danych o\n' +
-                'zanieczyszczeniu korzystającą z danych\n' +
-                'satelitarnych oraz sztucznej inteligencji.”',
+                '“In the category of Identifying air pollution sources, the ' +
+                '<strong>AI Embassy</strong> team turned out to be the best. ' +
+                'He presented an innovative method of analyzing pollution data ' +
+                'using satellite data and artificial intelligence. ”',
             source:
                 'https://firma.rp.pl/biznes/4431-nowe-technologie-dla-srodowiska-znamy-laureatow-smogathonu-2019',
             sourceName: 'firma.rp.pl',
